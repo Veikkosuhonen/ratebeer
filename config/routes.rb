5f0d6fe30f5a84223931238d48276f24b76ec24f
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-08-11 15:51:44 +0300

    Week 2 start

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
+  get 'ratings', to: 'ratings#index'
+  root 'breweries#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
