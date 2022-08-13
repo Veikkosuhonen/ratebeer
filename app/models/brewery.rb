e1122cb8b34f484d9e6d5648bdcf155315202a17
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-08-13 14:26:55 +0300

    2.14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,7 +1,12 @@
 class Brewery < ApplicationRecord
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
 
   def to_s
     name
+  end
+
+  def average_rating
+    ratings.average(:score)
   end
 end
