131d00bafda9aff5a21b616acb1e921c434d71d7
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-08-14 14:08:37 +0300

    3.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
   resources :ratings, only: [:index, :new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users#new'
 
   root 'breweries#index'
   # The priority is based upon order of creation: first created -> highest priority.
