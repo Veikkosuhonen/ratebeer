fd160c19583fa7e87291ff09ae40118f6a3c53c3
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-08-19 12:27:32 +0300

    3.8

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -6,7 +6,7 @@ class Brewery < ApplicationRecord
   validates :name, presence: true
   validates :year, numericality: {
     greater_than_or_equal_to: 1040,
-    less_than_or_equal_to: 2022,
+    less_than_or_equal_to: ->(_) { Time.now.year },
     only_integer: true
   }
 
