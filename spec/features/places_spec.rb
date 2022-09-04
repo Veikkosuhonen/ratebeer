2a8ebf897c9bc7ef7c263cce5e1f743a7ac182e2
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-09-04 15:35:55 +0300

    5.16

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 describe "Places" do
+
   it "if one is returned by the API, it is shown at the page" do
     allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
       [ Place.new( name: "Oljenkorsi", id: 1 ) ]
+    )
+    allow(WeatherApi).to receive(:weather_in).with("kumpula").and_return(
+      Weather.new(temperature: 28)
     )
 
     visit places_path
@@ -17,6 +21,9 @@ describe "Places" do
     allow(BeermappingApi).to receive(:places_in).with("merihaka").and_return(
       [ Place.new( name: "Meribar", id: 1 ), Place.new( name: "Sir Oliver", id: 2 ) ]
     )
+    allow(WeatherApi).to receive(:weather_in).with("merihaka").and_return(
+      Weather.new(temperature: 28)
+    )
 
     visit places_path
     fill_in('city', with: 'merihaka')
@@ -29,6 +36,9 @@ describe "Places" do
   it "if none are found by API, a message is shown" do
     allow(BeermappingApi).to receive(:places_in).with("tervalampi").and_return(
       [ ]
+    )
+    allow(WeatherApi).to receive(:weather_in).with("tervalampi").and_return(
+      Weather.new(temperature: -32)
     )
 
     visit places_path
